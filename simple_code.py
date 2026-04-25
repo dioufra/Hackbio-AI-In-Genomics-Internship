6ebdf4fc1fb1e769fee3f42d6d4e5359d600855a
François Pape Diouf <105300033+dioufra@users.noreply.github.com>
2026-04-25 08:52:50 +0100

    Add personal introduction in simple_code.py

--- simple_code.py
+++ simple_code.py
@@ -1,1 +1,9 @@
+name = "François Pape Diouf"
+affiliation = "Dakar Institute of technology"
+favorite_gene = "DREB1A"
+organism = "Arabidopsis thaliana"
 
+print(f"Hi, my name is {name}.")
+print(f"I am affiliated with {affiliation}.")
+print(f"My favorite gene is {favorite_gene}.")
+print(f"This gene is found in {organism}.")
